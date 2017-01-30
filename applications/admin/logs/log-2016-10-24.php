47b2186f352154945179065e647f4bebbeeb60a4
Fathoni <m.fathoni@mail.com>
2017-01-31 05:43:18 +0700

    Menghapus log

--- applications/admin/logs/log-2016-10-24.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2016-10-24 10:45:37 --> Severity: Warning --> Creating default object from empty value C:\Users\Fathoni\Documents\XAMPP\htdocs\ci-cbt-yufi\applications\shared\models\Jadwal_model.php 42
-ERROR - 2016-10-24 12:43:06 --> Severity: Notice --> Undefined index: ifs C:\Users\Fathoni\Documents\XAMPP\htdocs\ci-cbt-yufi\applications\admin\views_compiled\4d2a4c844623b8e7e7b6bcb74495a032cef9cd0f_0.file.index.tpl.php 51
-ERROR - 2016-10-24 12:43:50 --> Severity: Warning --> Creating default object from empty value C:\Users\Fathoni\Documents\XAMPP\htdocs\ci-cbt-yufi\applications\admin\models\Soal_model.php 92
-ERROR - 2016-10-24 12:43:50 --> Severity: Warning --> Creating default object from empty value C:\Users\Fathoni\Documents\XAMPP\htdocs\ci-cbt-yufi\applications\admin\models\Soal_model.php 105
