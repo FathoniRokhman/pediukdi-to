f5a3a809129e377c4dc78f8075c76eafb7cb9878
Fathoni <m.fathoni@mail.com>
2017-02-07 00:50:50 +0700

    ganti konfigurasi email

--- applications/user/config/email.php
+++ applications/user/config/email.php
@@ -6,9 +6,16 @@
 |--------------------------------------------------------------------------
 */
 
+/*
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'localhost';
 $config['smtp_user'] = 'no-reply@pediukdi.com';
 $config['smtp_pass'] = 'no-reply2016';
 $config['smtp_port'] = 25;
 $config['mailtype'] = 'html';
+*/
+
+$config['protocol'] = 'sendmail';
+$config['mailpath'] = '/usr/sbin/sendmail';
+$config['charset'] = 'iso-8859-1';
+$config['wordwrap'] = TRUE;
